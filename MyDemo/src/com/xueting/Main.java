909a2229e02255dd7b90fa83f7f7aed68de5cc01
AliceJiang <alice.jiang@codenewman.com>
2018-07-13 11:54:48 +0800

    第一次提交

---
+++ MyDemo/src/com/xueting/Main.java
@@ -0,0 +1,10 @@
+package com.xueting;
+
+/**
+ * @Date : 2018年07月13日 上午11:49
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
